8a18ee83c05caf42d77d3a0cb69dab841776409d
alvinChae <bhchae@iglim.net>
2022-12-09 15:33:43 +0900

    Chapter 7 Detecting shape and Indicate shape of object

--- Chapter7.cpp
+++ Chapter7.cpp
@@ -8,7 +8,7 @@ using namespace cv;
 using namespace std;
 
 
-/////////////   Images    ///////////////
+/////////////   Color Detection    ///////////////
 
 Mat imgGray, imgBlur, imgCanny, imgDil, imgErode;
 
@@ -27,16 +27,35 @@ void getContours(Mat imgDil, Mat img) {
 	
 		vector<vector<Point>> conPoly(contours.size());
 		vector<Rect> boundRect(contours.size());
+		string objectType;
 		
 		// exclusive noise
 		if (area > 1000)
 		{
 			float peri = arcLength(contours[i], true);
 			approxPolyDP(contours[i], conPoly[i], 0.02*peri, true);
-			drawContours(img, conPoly, i, Scalar(255, 0, 255), 2);
+			
 			cout << conPoly[i].size() << endl;
 			boundRect[i] = boundingRect(conPoly[i]);
 			rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 3);
+			int objCor = (int)conPoly[i].size();
+
+			if (objCor == 3) { objectType = "Tri"; } 
+			else if (objCor == 4) 
+			{
+				float aspRatio = (float)boundRect[i].width / (float)boundRect[i].height;
+				cout << aspRatio << endl;
+				if (aspRatio > 0.95 && aspRatio < 1.05){objectType = "Square";}
+				else { objectType = "Rect"; }
+				
+			}
+			else if (objCor > 4) { objectType = "Circle"; }
+
+			drawContours(img, conPoly, i, Scalar(255, 0, 255), 2);
+			rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 3);
+			putText(img, objectType, { boundRect[i].x,boundRect[i].y - 5 }, FONT_HERSHEY_DUPLEX, 0.5, Scalar(0, 69, 255), 2);
+
+
 		}
 	}
 }
