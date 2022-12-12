fdb77d8268e3285391f41b23bb8642845298cb17
alvinChae <bhchae@iglim.net>
2022-12-12 16:10:44 +0900

    FEAT: Reorder of vertex's index

---
+++ Project2.cpp
@@ -0,0 +1,117 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+/////////////   Images    ///////////////
+
+Mat imgOriginal, imgGray, imgBlur, imgCanny, imgThre, imgDil, imgErode;
+vector<Point> initialPoints, docPoints;
+
+Mat preProcessing(Mat img)
+{
+	cvtColor(img, imgGray, COLOR_BGR2GRAY);
+	GaussianBlur(imgGray, imgBlur, Size(3, 3), 3, 0);
+	Canny(imgBlur, imgCanny, 25, 75);
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+	dilate(imgCanny, imgDil, kernel);
+	// erode(imgDil, imgErode, kernel);
+
+	return imgDil;
+}
+
+vector<Point> getContours(Mat imgDil) {
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+
+	findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	// drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
+
+	vector<vector<Point>> conPoly(contours.size());
+	vector<Rect> boundRect(contours.size());
+
+	vector<Point> biggest;
+	int maxArea = 0;
+
+	for (int i = 0; i < contours.size(); i++)
+	{
+		int area = contourArea(contours[i]);
+		cout << area << endl;
+
+
+		string objectType;
+
+		// exclusive noise
+		if (area > 1000)
+		{
+			float peri = arcLength(contours[i], true);
+			approxPolyDP(contours[i], conPoly[i], 0.02*peri, true);
+
+			if (area > maxArea && conPoly[i].size() == 4) {
+
+				drawContours(imgOriginal, conPoly, i, Scalar(255, 0, 255), 5);
+				
+				biggest = { conPoly[i][0],conPoly[i][1], conPoly[i][2], conPoly[i][3] };
+				maxArea = area;
+
+			}
+
+			//drawContours(imgOriginal, conPoly, i, Scalar(255, 0, 255), 2);
+			//rectangle(imgOriginal, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 3);
+		}
+	}
+	return biggest;
+}
+
+void drawPoints(vector<Point> points, Scalar color)
+{
+	for (int i = 0; i < points.size(); i++)
+	{
+		circle(imgOriginal, points[i], 10, color, FILLED);
+		putText(imgOriginal, to_string(i), points[i], FONT_HERSHEY_PLAIN, 4, color, 2);
+	}
+}
+
+vector<Point> reorder(vector<Point> points)
+{
+	vector<Point> newPoints;
+	vector<int> sumPoints, subPoints;
+	
+	for (int i = 0; i < 4; i++)
+	{
+		sumPoints.push_back(points[i].x + points[i].y);
+		subPoints.push_back(points[i].x - points[i].y);
+
+	}
+
+	newPoints.push_back(points[min_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); //0
+	newPoints.push_back(points[max_element(subPoints.begin(), subPoints.end()) - subPoints.begin()]); //2
+	newPoints.push_back(points[min_element(subPoints.begin(), subPoints.end()) - subPoints.begin()]); //2
+	newPoints.push_back(points[max_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); //3
+
+	return newPoints;
+}
+
+void main() {
+	string path = "Resources/paper.jpg";
+	imgOriginal = imread(path);
+	resize(imgOriginal, imgOriginal, Size(), 0.5, 0.5);
+	
+	// preprocessing
+	imgThre = preProcessing(imgOriginal);
+	// Get Contours - Biggest
+	initialPoints = getContours(imgThre);
+	//drawPoints(initialPoints, Scalar(0, 0, 255));
+	docPoints = reorder(initialPoints);
+	drawPoints(docPoints, Scalar(0, 255, 0));
+	// Warp
+
+	imshow("Image", imgOriginal);
+	imshow("Image Dilation", imgThre);
+	waitKey(0);
+}
