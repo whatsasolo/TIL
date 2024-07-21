8ba38cf146ab2c71eb8a85342629a1bea9fdc6e6
whatsasolo <dowsing.stealth-0d@icloud.com>
2024-07-22 00:40:39 +0900

    클래스 메서드

--- Unity/ConsoleApp1/ConsoleApp1/Program.cs
+++ Unity/ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace ConsoleApp1
 {
+    // Methode (함수)
+    // 1. 정리하기 위해, 2. 반복을 단순화
     class GiftBox
     {
         public string Letter;
@@ -12,35 +14,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+            GiftBox addressA = GiftBoxMaker("A 하이", 100000);
+            GiftBox addressB = GiftBoxMaker("B 하이", 110000);
+            GiftBox addressC = GiftBoxMaker("C 하이", 120000);
+        }
+
+        private static GiftBox GiftBoxMaker(string letter, int money)
+        {
             // 인스턴스 생성
-            GiftBox addressA = new GiftBox()
+            GiftBox address = new GiftBox()
             {
-                Letter = "하이",
-                Money = 100000,
+                Letter = letter,
+                Money = money,
             };
-
-            GiftBox addressB = new GiftBox()
-            {
-                Letter = "하이",
-                Money = 100000,
-            };
-
-            GiftBox addressC = new GiftBox()
-            {
-                Letter = "하이",
-                Money = 100000,
-            };
-            Console.WriteLine(a.Letter);
-            Console.WriteLine(a.Money);
-            // 인스턴스 주소 전달
-            GiftBox b = a;
-            
-            // 인스턴스 내용 변경
-            a.Letter = "잘 니내냐";
-            a.Money = 150000;
-
-            Console.WriteLine(a.Letter);
-            Console.WriteLine(a.Money);
+            return address;
         }
     }
 }
