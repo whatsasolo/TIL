39d51893a7cd943e55d2e94b4c32ad5a023c850e
whatsasolo <dowsing.stealth-0d@icloud.com>
2024-07-22 00:33:11 +0900

    클래스, 인스턴스 생성

--- Unity/ConsoleApp1/ConsoleApp1/Program.cs
+++ Unity/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ConsoleApp1
 {
+    class GiftBox
+    {
+        public string Letter;
+        public int Money;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 인스턴스 생성
+            GiftBox addressA = new GiftBox()
+            {
+                Letter = "하이",
+                Money = 100000,
+            };
+
+            GiftBox addressB = new GiftBox()
+            {
+                Letter = "하이",
+                Money = 100000,
+            };
+
+            GiftBox addressC = new GiftBox()
+            {
+                Letter = "하이",
+                Money = 100000,
+            };
+            Console.WriteLine(a.Letter);
+            Console.WriteLine(a.Money);
+            // 인스턴스 주소 전달
+            GiftBox b = a;
+            
+            // 인스턴스 내용 변경
+            a.Letter = "잘 니내냐";
+            a.Money = 150000;
+
+            Console.WriteLine(a.Letter);
+            Console.WriteLine(a.Money);
         }
     }
 }
